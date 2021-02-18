c8c227192eedae9ce0c4df76f89f24550903929b
pimpim888 <79275235+pimpim888@users.noreply.github.com>
2021-02-18 13:46:02 -0400

    Delete Komputer directory

--- Komputer/suporte.java
+++
@@ -1,5 +0,0 @@
-Pc = komputer * (darkbot3.0 + darkbot - dark)
-           + 4 * bot; // PREFER
-
-Android = /0/android/suporte.java * (darkbot3.0 + dark
-                       - darkbot) + 4 * bot; // AVOID
