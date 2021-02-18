7699d329cfacedc1634a2c51451cc0c3d0c6b4ff
pimpim888 <79275235+pimpim888@users.noreply.github.com>
2021-02-18 13:38:14 -0400

    Add files via upload

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (pimpimbot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (pimpim )
+ * Copyright notice      (bot com suporte a Android/Java)
